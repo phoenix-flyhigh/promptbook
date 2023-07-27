5964aa90278a577a498435a050d64b5200ba14e6
Akshaya <j.akshaya07@gmail.com>
2023-07-27 20:18:11 +0530

    Add. Basic home page

--- app/__test__/home.spec.tsx
+++ app/__test__/home.spec.tsx
@@ -2,11 +2,19 @@ import { render, screen } from '@testing-library/react'
 import Home from '../page'
 
 describe('Home page tests', () => {
-  it('renders a heading', () => {
+  it('Should render a title', () => {
     render(<Home />)
 
-    const heading = screen.getByText("Home")
+    const title: HTMLElement = screen.getByTestId("tid-title")
 
-    expect(heading).toBeInTheDocument()
+    expect(title).toBeInTheDocument()
+  })
+
+  it('Should render a description', () => {
+    render(<Home />)
+
+    const description: HTMLElement = screen.getByTestId("tid-description")
+
+    expect(description).toBeInTheDocument()
   })
 })
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
-
+const Home: () => JSX.Element = () => (
+  <section className="w-full flex-col flex-center">
+    <h1 className="head_text text_center" data-testid="tid-title">
+      Discover and Share
+      <br className="max-md:hidden" />
+      <span className="orange_gradient text-center">AI Powered Prompts</span>
+    </h1>
+    <p className="desc text-center" data-testid="tid-description">
+      Promptopia is an open-source AI prompting tool for modern world to
+      discover, create and share creative prompts
+    </p>
+  </section>
+)
 export default Home
