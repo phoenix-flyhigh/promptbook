80c1ddd00f6c6d9ab21cdd857d42dbb9ecb1a016
Akshaya <j.akshaya07@gmail.com>
2023-07-31 20:58:45 +0530

    Add. Test for prompt service

---
+++ utils/__test__/PromptService.spec.ts
@@ -0,0 +1,54 @@
+import PromptService from "../PromptService"
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+import { CreatePromptRequest } from "@/app/create-prompt/page";
+
+const handlers = [
+    rest.post("/api/prompt/new", async (req, res, ctx) => {
+        const {userId} : CreatePromptRequest = await req.json()
+
+        if (userId === "")
+            return res(ctx.status(401), ctx.json({ message: "Unauthorised"}));
+        return res(ctx.status(200), ctx.json({ prompt: "s", tag: "s" }));
+    }),
+];
+
+const server = setupServer(...handlers);
+
+describe("Prompt service tests", () => {
+    beforeAll(() => server.listen());
+    afterEach(() => {
+        server.restoreHandlers();
+    });
+
+    afterAll(() => server.close());
+
+    it("Should return success response when api request succeeds", async () => {
+        const response = await PromptService.postPrompt({
+            prompt: "s",
+            userId: "7",
+            tag: "s"
+        });
+
+        expect(response).toEqual({
+            prompt: "s",
+            tag: "s"
+        })
+    })
+
+    it("Should throw error when api request fails", async () => {
+        let requestFailed = false;
+        
+        try {
+            await PromptService.postPrompt({
+                prompt: "s",
+                userId: "",
+                tag: "s"
+            }) 
+        } catch (error) {
+            requestFailed = true
+        }
+
+        expect(requestFailed).toBeTruthy()
+    })
+})
